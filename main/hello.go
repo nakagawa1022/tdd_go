0475c7418e58354343543bb66d2583dbdf355e09
nakagawa1022 <129717557+nakagawa1022@users.noreply.github.com>
2023-07-30 19:39:49 +0900

    tdd勉強用

---
+++ main/hello.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+const englishHelloPrefix = "Hello, "
+const spanishHelloPrefix = "Hola, "
+const frenchHelloPrefix = "Bonjour, "
+
+func greetingPrefix(language string) string {
+	prefix := englishHelloPrefix
+	switch language {
+	case "Spanish":
+		prefix = spanishHelloPrefix
+	case "French":
+		prefix = frenchHelloPrefix
+	}
+	return prefix
+}
+
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	prefix := greetingPrefix(language)
+
+	return prefix + name
+
+}
+
+func main() {
+	fmt.Println(Hello("Chris", ""))
+}
---
+++ main/hello_test.go
@@ -0,0 +1,41 @@
+package main
+
+import "testing"
+
+func TestHello(t *testing.T) {
+
+	asserCorrectMessage := func(t *testing.T, got, want string) {
+		t.Helper()
+		if got != want {
+			t.Errorf("got %q want %q", got, want)
+		}
+	}
+
+	t.Run("渡された引数に対してHelloという", func(t *testing.T) {
+
+		got := Hello("Chris", "")
+		want := "Hello, Chris"
+
+		asserCorrectMessage(t, got, want)
+	})
+
+	t.Run("何も渡されなかった時,Hello, Worldという", func(t *testing.T) {
+		got := Hello("", "")
+		want := "Hello, World"
+
+		asserCorrectMessage(t, got, want)
+	})
+
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		asserCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Frenchman", "French")
+		want := "Bonjour, Frenchman"
+
+		asserCorrectMessage(t, got, want)
+	})
+}
